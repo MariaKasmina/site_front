f822f94aefce4579689e141fb5aae4125ecd9871
Maria <marijakasmina99@gmail.com>
2021-02-11 15:44:50 +0300

    restructure CarouselFeature.js

--- src/Components/CarouselFeature.js
+++ src/Components/CarouselFeature.js
@@ -4,42 +4,34 @@ import caruselSlideSecond from "../images/slide22.jpg";
 import caruselSlideThird from "../images/slide3.jpg";
 import React from "react";
 
-function CarouselFeature(){
-    return(
-        <Carousel>
-            <Carousel.Item>
-                <img
-                    className="slide-image"
-                    src={caruselSlideFirst}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3 className='slide-title'>Путешествуйте вместе с нами</h3>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="slide-image"
-                    src={caruselSlideSecond}
-                    alt="Second slide"
-                />
+const carouselItems = [
+    {
+        title: 'Путешествуйте вместе с нами',
+        img: caruselSlideFirst
+    },
+    {
+        title: 'Только самые выгодные предложения',
+        text: '',
+        img: caruselSlideSecond
+    },
+    {
+        title: 'Исполняем Ваши мечты о путешествиях',
+        text: 'Розыгрыши путевок каждый месяц',
+        img: caruselSlideThird
+    }
+]
 
-                <Carousel.Caption>
-                    <h3>Только самые выгодные предложения</h3>
-                    <p>Более 1000 выгодных предложений ждут Вас</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="slide-image"
-                    src={caruselSlideThird}
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h3>Исполняем Ваши мечты о путешествиях</h3>
-                    <p>Розыгрыши путевок каждый месяц</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+function CarouselFeature() {
+    return (
+        <Carousel>
+            {carouselItems.map((element, key) => (
+                <Carousel.Item key={key}>
+                    <Carousel.Caption>
+                        <h3 className='slide-title'>{element.title}</h3>
+                        <p>{element.text}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            ))}
         </Carousel>
     );
 }
